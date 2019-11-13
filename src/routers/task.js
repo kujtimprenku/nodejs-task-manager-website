965955a8ea7f1c70f4edeedc6b1ab3451163766a
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-13 21:31:46 +0100

    Add sorting to tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,12 +18,16 @@ router.post('', auth, async (req, res) => {
 });
 
 router.get('', auth, async (req, res) => {
-    const match = {
-
-    };
+    const match = {};
+    const sort = {};
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true';
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
     try {
         await req.user.populate({
@@ -32,6 +36,7 @@ router.get('', auth, async (req, res) => {
             options: {
                 limit: parseInt(req.query.limit),
                 skip: parseInt(req.query.skip),
+                sort,
             }
         }).execPopulate();
         res.send(req.user.tasks);
