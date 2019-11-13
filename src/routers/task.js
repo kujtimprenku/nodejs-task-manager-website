9b866a596c357076a6440816d028bd3e0c9446ed
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-13 21:03:13 +0100

    Filter tasks by completed

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,8 +18,18 @@ router.post('', auth, async (req, res) => {
 });
 
 router.get('', auth, async (req, res) => {
+    const match = {
+
+    };
+
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
     try {
-        const tasks = await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path: 'tasks',
+            match,
+        }).execPopulate();
         res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
