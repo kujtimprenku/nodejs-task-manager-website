3b4f40488b3ec70f9bbb5882544b2c62bde2c2a3
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-13 18:27:19 +0100

    Add logout/logoutAll routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -29,6 +29,28 @@ router.post('/login', async (req, res) => {
     }
 });
 
+router.post('/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+           return token.token !== req.token;
+        });
+        await req.user.save();
+        res.send();
+    } catch (e) {
+        res.status(500).send();
+    }
+});
+
+router.post('/logoutAll', auth, async (req, res) => {
+   try {
+       req.user.tokens =[];
+       await req.user.save();
+       res.send();
+   } catch (e) {
+       res.status(500).send();
+   }
+});
+
 router.get('/me', auth, async (req, res) => {
     res.send(req.user);
 });
