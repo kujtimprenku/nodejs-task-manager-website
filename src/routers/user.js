83fcd003c70c1f13a44dad098b9f98279122be0a
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2019-11-13 18:48:58 +0100

    Hide user private data on response

--- src/routers/user.js
+++ src/routers/user.js
@@ -20,10 +20,8 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         const token = await user.generateAuthToken();
-        res.send({
-            user: user,
-            token: token,
-        });
+
+        res.send({user, token});
     } catch (e) {
         res.status(400).send();
     }
